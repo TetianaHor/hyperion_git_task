a427848d6ea5eda1cf1e649db16fa7a305bff3e7
Tetiana <tetiana@you-agency.com>
2024-03-29 16:26:08 +0000

    input a decorator symbol

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,9 @@
 def custom_print(string, decorator):
+    '''Custom print function'''
     print(f"{decorator * 3} {string} {decorator * 3}")
 
+# Ask user to type some text and a symbol
 text = input("Type something: ")
-custom_print(text, '*')
+symbol = input("Choose a string decoration symbol: ")
+# Print out a decorated string
+custom_print(text, symbol)

488b639f689f7df821a39160cf23d7b1110daf0f
Tetiana <tetiana@you-agency.com>
2024-03-29 17:02:16 +0000

    add decoration options

--- helloWorld.py
+++ helloWorld.py
@@ -2,8 +2,16 @@ def custom_print(string, decorator):
     '''Custom print function'''
     print(f"{decorator * 3} {string} {decorator * 3}")
 
-# Ask user to type some text and a symbol
+decoration_list = {0: '*', 1: '\"', 2: '_', 3: '$'}
+# Ask user to type some text
 text = input("Type something: ")
-symbol = input("Choose a string decoration symbol: ")
+# Ask user to choose a decoration symbol
+print("You can select one of those options as a decoration symbol")
+for index, symbol in decoration_list.items():
+    print(f"{index} for {symbol}")
+symbol_key = input("Choose a string decoration symbol: ")
 # Print out a decorated string
-custom_print(text, symbol)
+if symbol_key.isdigit() and int(symbol_key) in decoration_list:
+    custom_print(text, decoration_list[int(symbol_key)])
+else:
+    print("Wrong key")
